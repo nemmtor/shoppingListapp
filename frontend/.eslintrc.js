aab79ff8cce4e531e40c9f208f82f5125ba7d759
Kacper Witas <kacper0witas@gmail.com>
2020-12-23 21:21:16 +0100

    Adds eslint and prettier to frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  plugins: ['react', '@typescript-eslint', 'no-loops', 'prettier'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    createDefaultProgram: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'prettier/prettier': 'error',
+    'no-loops/no-loops': 2,
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+  },
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'fontsource-roboto';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );

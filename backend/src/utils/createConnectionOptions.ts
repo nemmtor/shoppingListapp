591a2ea3275014e47e984e3fcdb1d871cabba901
Kacper Witas <kacper0witas@gmail.com>
2021-01-06 01:58:43 +0100

    Adds docker dev env

--- backend/src/utils/createConnectionOptions.ts
+++ backend/src/utils/createConnectionOptions.ts
@@ -3,12 +3,10 @@ import { ConnectionOptions, getConnectionOptions } from 'typeorm';
 import { config } from '../config';
 
 const { IS_PROD } = config;
-
 export const createConnectionOptions = async (): Promise<ConnectionOptions> => {
   const connectionOptions = await getConnectionOptions();
   const extraOptions = {
-    // maybe this should be 'db'
-    host: IS_PROD ? 'shopping-db' : 'localhost',
+    host: IS_PROD ? process.env.TYPEORM_HOST : 'dev.shopping-db',
     synchronize: !IS_PROD,
   };
   Object.assign(connectionOptions, extraOptions);
---
+++ env.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    POSTGRES_PASSWORD: string;
+  }
+}

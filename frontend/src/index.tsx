678a7a0a3bdfb3c2ddadccd9f8b157da1ce43ff7
Kacper Witas <kacper0witas@gmail.com>
2021-01-19 14:30:53 +0100

    Adds load env

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'fontsource-roboto';
+import dotenv from 'dotenv';
 
 import App from './App';
 import './styles/index.scss';
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
